1fd58a24007e465f4e73b2ad23ff2e59f2713974
Naveenkumar <120003555+KATHIRNEEVAN@users.noreply.github.com>
2024-05-07 15:11:12 +0530

    passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -1,18 +1,46 @@
-import random
 import tkinter as tk
+import random
+import string
 
-def generate_password():
-    password_length = 8  # Length of the password
-    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+"
-    password = ''.join(random.choice(characters) for _ in range(password_length))
-    password_label.config(text=password)
+
+
+def generate_password(length):
+    if length.isdigit():
+        chars = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(chars) for _ in range(int(length)))
+        return password
+    else:
+        return 'Invalid Input'
+
+def validate_length(text):
+    if text == '':
+        return True
+    elif text.isdigit():
+        return int(text) > 0
+    else:
+        return False
+
+
+def invalid_length():
+    label.config(text='Invalid Input', fg='red')
 
 window = tk.Tk()
+window.title('Password Generator')
+window.geometry('400x400')
+window.configure(bg='orange', padx=20, pady=20)
 
-generate_button = tk.Button(window, text="Generate Password", command=generate_password)
-generate_button.pack()
+entry_label = tk.Label(window, text='Enter password length:', font=('serif', 20), bg='light blue', fg='black')
+entry_label.pack(side=tk.TOP, pady=10)
 
-password_label = tk.Label(window, text="")
-password_label.pack()
+pass_entry = tk.Entry(window, width=20, font=('Fantasy', 20), bg='light blue', fg='black', textvariable="")
+pass_entry.pack(side=tk.TOP, pady=10)
+
+pass_entry.config(validate='focusout', validatecommand=(window.register(validate_length), '%P'), invalidcommand=invalid_length)
+
+label = tk.Label(window, text='Password', font=('serif', 20), bg='light blue', fg='black')
+label.pack(side=tk.TOP, pady=10)
+
+button = tk.Button(window, text='Generate Password', font=('serif', 20), bg='light blue', fg='black', command=lambda: label.config(text=generate_password(pass_entry.get()), fg='dark green'))
+button.pack(side=tk.TOP, pady=10)
 
 window.mainloop()
